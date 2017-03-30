6a4e2cd0e8f765541859d547476e9b801f3f0fe4
Agustin <avaleriani@users.noreply.github.com>
2017-03-30 16:04:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+var https = require('https');
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var options = {
+  key: fs.readFileSync('./privatekey.pem'),
+  cert: fs.readFileSync('./server.crt')
+};
 app.use('/', express.static(__dirname + '/'));
-app.listen(8080, function() { console.log('listening')});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpServer.listen(8080);
+httpsServer.listen(8443);
