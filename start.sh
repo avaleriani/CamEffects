a46a66749345b643e5d09f4214cc96177b783395
Agustin Valeriani <agvaleriani@gmail.com>
2017-03-30 21:29:17 -0300

    test export.

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 cd /etc/webapp/
+echo "Installing pm2"
 sudo npm install pm2 -g
+echo whereis pm2
+echo "Installing yarn"
 sudo npm install yarn -g
+echo whereis yarn
 export PATH=$PATH:whereis pm2
 export PATH=$PATH:whereis yarn
 source ~/.bashrc
 sudo yarn install
 sudo pm2 start server.js
+
