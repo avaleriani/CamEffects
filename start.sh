79919cacd3d784d15bc396bc033a15629a4f50e5
Agustin Valeriani <agvaleriani@gmail.com>
2017-03-30 21:46:19 -0300

    test server deploy

--- start.sh
+++ start.sh
@@ -2,15 +2,7 @@
 cd /etc/webapp/
 echo "Installing pm2"
 sudo npm install pm2 -g
-pathpm2=`whereis -b pm2`
-echo "Pm2 yarn: $pathpm2"
-echo "Installing yarn"
-sudo npm install yarn -g
-pathYarn=`whereis -b yarn`
-echo "Path yarn: $pathYarn"
-export PATH=$PATH:pathpm2
-export PATH=$PATH:pathYarn
-source ~/.bashrc
-sudo yarn install
-sudo pm2 start server.js
+echo "Installing npm"
+sudo npm install
+sudo /usr/local/bin/pm2 start server.js
 
